939e3554f006ee36b6f7a0c7cb30cbfce8caeca1
Oleg Andreyev <oleg@andreyev.lv>
2020-04-28 02:08:40 +0300

    improving php-build for osx
    - adding condition patches (required for osx)
    - extracted uname result into variable outside of is_osx to avoid calling it N-time
    - same about osx_version_string
    - added verbosity to patch method and making sure if error occurs it will fail
    - linking as much as possible from OSX bundled libs
    - fixing icu-config version compare when it has decimal (.) value

--- share/php-build/after-patch.d/macos.sh
+++ share/php-build/after-patch.d/macos.sh
@@ -1,10 +1,8 @@
-#see https://github.com/php/php-src/commit/e28db2276b3d73bcdbf256bd7efffff3e8f874e0
-# applying this patch to lower versions
+#  see https://github.com/php/php-src/commit/e28db2276b3d73bcdbf256bd7efffff3e8f874e0
+# after appling patch need to rebuild configure
 
 if is_osx; then
-  if [[ "$DEFINITION" =~ ^("7.0.".*)$ || "$DEFINITION" =~ ^("7.1.".*)$ || "$DEFINITION" =~ ^("7.2.".*)$ || "$DEFINITION" =~ ^("7.3.".*)$ ]]; then
-    echo $DEFINITION
-     sed -i -E 's#test -f \$ICONV_DIR/\$PHP_LIBDIR/lib\$iconv_lib_name.\$SHLIB_SUFFIX_NAME$#test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.$SHLIB_SUFFIX_NAME || test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.tbd#g' acinclude.m4
+  if [[ "$DEFINITION" =~ ^("7.2.".*)$ ]]; then
     # regenerate ./configure after patching
     ./buildconf --force
   fi;
--- tests/xdebug.bats
+++ tests/xdebug.bats
@@ -4,14 +4,14 @@
     local xdebug_installed="$(echo "$DEFINITION_CONFIG" | grep '^install_xdebug')"
     local ext_dir=$("$TEST_PREFIX/bin/php" -r "echo ini_get('extension_dir');")
 
-    [ -z $xdebug_installed ] && skip "Xdebug isn't installed"
+    [ -z "$xdebug_installed" ] && skip "Xdebug isn't installed"
     [ -f "$ext_dir/xdebug.so" ]
 }
 
 @test "Enables Xdebug" {
     local xdebug_installed="$(echo "$DEFINITION_CONFIG" | grep '^install_xdebug')"
 
-    [ -z $xdebug_installed ] && skip "Xdebug isn't installed"
+    [ -z "$xdebug_installed" ] && skip "Xdebug isn't installed"
     "$TEST_PREFIX/bin/php" -i | grep "xdebug"
 }
 
