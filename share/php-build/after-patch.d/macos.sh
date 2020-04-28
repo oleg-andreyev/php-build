73eb188d72a8424ee6b9b77ac6db00bd0e03d192
Oleg Andreyev <oleg@andreyev.lv>
2020-04-28 22:53:08 +0300

    removed after/before patches logic in favor of platform patch

--- share/php-build/after-patch.d/macos.sh
+++
@@ -1,9 +0,0 @@
-#  see https://github.com/php/php-src/commit/e28db2276b3d73bcdbf256bd7efffff3e8f874e0
-# after appling patch need to rebuild configure
-
-if is_osx; then
-  if [[ "$DEFINITION" =~ ^("7.2.".*)$ ]]; then
-    # regenerate ./configure after patching
-    ./buildconf --force
-  fi;
-fi;
--- share/php-build/before-patch.d/macos.sh
+++
@@ -1,6 +0,0 @@
-#see https://github.com/php/php-src/commit/e28db2276b3d73bcdbf256bd7efffff3e8f874e0
-# applying this patch to lower versions
-
-if is_osx && [[ "$DEFINITION" =~ ^("7.2.".*)$ ]]; then
-  patch_file "macos-php-7.2.patch"
-fi;
